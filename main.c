bd8f344bf9969f0b595af05b66c2e44ed87d0fd6
JADEWANG <jbwang@mtu.edu>
2021-10-06 10:51:40 -0400

    TODO: Figure out how to read file from stdin.

--- main.c
+++ main.c
@@ -7,10 +7,18 @@
 
 int main(int argc, char* argv[]) {
     //Test for correct arguments (0)
-    if (argv > 0) {
-        printf("ERROR: Too many arguments");
+    if (argc > 1) {
+        printf("ERROR: Too many arguments\n");
         return -1;
     }
+
+    char* input;
+    scanf("%ms", input);
+
+    printf("Argc = %d\n", argc);
+    printf("Input: %s\n", input);
+    free(input);
+
 
     return 0;
 }
