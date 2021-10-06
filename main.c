a95666783891ada1e95c45e6f5e8b6dc998ba053
JADEWANG <jbwang@mtu.edu>
2021-10-06 10:36:11 -0400

    Testing Something...

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char* argv[]) {
+    //Test for correct arguments (0)
+    if (argv > 0) {
+        printf("ERROR: Too many arguments");
+        return -1;
+    }
+
     return 0;
 }
