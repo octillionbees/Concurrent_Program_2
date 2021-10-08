082ecb7ceea9675306579da8b104735648a5e3b2
JADEWANG <jbwang@mtu.edu>
2021-10-08 11:54:21 -0400

    implemented shared memory + partitioning algorithm for qsort

--- qsort.c
+++ qsort.c
@@ -9,6 +9,8 @@
 #include <unistd.h>
 #include <string.h>
 
+int partition(int* a, int l, int r);
+void swap(int* a, int i, int j);
 
 int main(int argc, char* argv[]) {
     char buf[100];
@@ -22,12 +24,39 @@ int main(int argc, char* argv[]) {
 
     int left = atoi(argv[1]);
     int right = atoi(argv[2]);
-    int key = atoi(argv[3]);
+    int k = atoi(argv[3]);
+    int key = atoi(argv[4]);
 
     //attach shared memory
-
+    int shm_id = shmget(key, sizeof(int) * k, 0666); //shm only created by main
+    int* a = (int*) shmat(shm_id, NULL, 0); //attach shared memory to pointer a
     //partitioning algorithm
 
-    //wait, and exit
+    partition(a, left, right);
 
+    //wait
+
+    //detach shared memory (don't remove, main will remove)
+
+}
+
+int partition(int* a, int l, int r){
+    int p = a[l];
+    int s = l;
+
+    for (int i = l + 1; i <= r; i++) {
+        if (a[i] < p) {
+            s++;
+            swap(a, s, i);
+        }
+    }
+    swap(a, l, s);
+
+    return s;
+}
+
+void swap(int* a, int i, int j) {
+    int tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
 }
